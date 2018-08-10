7554feccb075b5a97eb8c6b75b28ccd6736edb9d
jpch89 <jpch89@outlook.com>
2018-08-10 17:18:13 +0800

    update to 3-2

--- fishbook.py
+++ fishbook.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 # @Author: jpch89
-# @Email:  jpch89@outlook.com
 # @Time:   2018/8/10 11:30
 
 from flask import Flask
-# from config import DEBUG
+# from flask import make_response
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -12,9 +11,13 @@ print(app.config['DEBUG'])
 
 @app.route('/hello/')
 def hello():
-    return "Hello, World!"
-
-# app.add_url_rule('/hello/', view_func=hello)
+    headers = {
+        'content-type': 'text/plain',
+        'location': 'http://www.bing.com'
+    }
+    # response = make_response('<html></html>', 404)
+    # response.headers = headers
+    return '<html></html>', 301, headers
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=81, debug=app.config['DEBUG'])
